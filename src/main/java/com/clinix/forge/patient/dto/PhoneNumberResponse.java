a89499e07f256992da690312449eab8ae1ab8017
RUT KOTICHA <153355434+RutKoticha@users.noreply.github.com>
2026-06-30 21:15:43 +0530

    Add schema descriptions

--- src/main/java/com/clinix/forge/patient/dto/PhoneNumberResponse.java
+++ src/main/java/com/clinix/forge/patient/dto/PhoneNumberResponse.java
@@ -1,13 +1,38 @@
 package com.clinix.forge.patient.dto;
 
 import com.clinix.forge.patient.entity.PhoneType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 
 public record PhoneNumberResponse(
+        @Schema(
+                name = "Id",
+                description = "System id for phone number"
+        )
         Long id,
+
+        @Schema(
+                name = "Phone Number",
+                description = "Phone number value"
+        )
         String phoneNumber,
+
+        @Schema(
+                name = "Phone Number Type",
+                description = "Type of the phone number. Can be PRIMARY / SECONDARY"
+        )
         PhoneType type,
+
+        @Schema(
+                name = "Created At",
+                description = "The system metadata for when the phone number record was created"
+        )
         Instant createdAt,
+
+        @Schema(
+                name = "Updated At",
+                description = "The system metadata for when the phone number record was last updated"
+        )
         Instant updatedAt
 ) {}
