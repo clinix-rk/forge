5b4423b37bcf6f0b30003ea3e9768587c7395919
RUT KOTICHA <153355434+RutKoticha@users.noreply.github.com>
2026-07-01 11:16:44 +0530

    Remove names from the response schema

--- src/main/java/com/clinix/forge/patient/dto/PatientResponse.java
+++ src/main/java/com/clinix/forge/patient/dto/PatientResponse.java
@@ -10,31 +10,26 @@ import java.util.Set;
 
 public record PatientResponse(
         @Schema(
-                name = "Patient id",
                 description = "System id for patient record"
         )
         Long id,
 
         @Schema(
-                name = "Case Number",
                 description = "Case number of the patient"
         )
         String caseNo,
 
         @Schema(
-                name = "Full name",
                 description = "Full name of the patient"
         )
         String name,
 
         @Schema(
-                name = "Date of Birth",
                 description = "Patient's date of birth to mainly calculate age"
         )
         LocalDate dateOfBirth,
 
         @Schema(
-                name = "Gender",
                 description = "Patients gender (can be MALE, FEMALE or OTHER)"
         )
         Gender gender,
@@ -46,55 +41,46 @@ public record PatientResponse(
         String email,
 
         @Schema(
-                name = "Address",
                 description = "Patient home address"
         )
         String address,
 
         @Schema(
-                name = "City",
                 description = "Residence city"
         )
         String city,
 
         @Schema(
-                name = "Pincode",
                 description = "Postal code for the address"
         )
         String pincode,
 
         @Schema(
-                name = "Referred By",
                 description = "The doctor whose refrence was brought by the patient"
         )
         String referredBy,
 
         @Schema(
-                name = "Phone Numbers",
                 description = "List of Primary and Secondary phone numbers"
         )
         List<PhoneNumberResponse> phoneNumbers,
 
         @Schema(
-                name = "Medical Conditions",
                 description = "Patient's medical conditions"
         )
         Set<String> medicalConditions,
 
         @Schema(
-                name = "Drug Allergies",
                 description = "Patient's drug allergies"
         )
         Set<String> drugAllergies,
 
         @Schema(
-                name = "Created At",
                 description = "The system metadata for when the patient record was created"
         )
         Instant createdAt,
 
         @Schema(
-                name = "Updated At",
                 description = "The system metadata for when the patient record was last updated"
         )
         Instant updatedAt
--- src/main/java/com/clinix/forge/patient/dto/PhoneNumberResponse.java
+++ src/main/java/com/clinix/forge/patient/dto/PhoneNumberResponse.java
@@ -7,31 +7,26 @@ import java.time.Instant;
 
 public record PhoneNumberResponse(
         @Schema(
-                name = "Id",
                 description = "System id for phone number"
         )
         Long id,
 
         @Schema(
-                name = "Phone Number",
                 description = "Phone number value"
         )
         String phoneNumber,
 
         @Schema(
-                name = "Phone Number Type",
                 description = "Type of the phone number. Can be PRIMARY / SECONDARY"
         )
         PhoneType type,
 
         @Schema(
-                name = "Created At",
                 description = "The system metadata for when the phone number record was created"
         )
         Instant createdAt,
 
         @Schema(
-                name = "Updated At",
                 description = "The system metadata for when the phone number record was last updated"
         )
         Instant updatedAt
